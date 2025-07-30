fc6e8eb025921c9aa78ccf595d3d9bd315207451
ykm <vldqmfqhr88@naver.com>
2025-07-30 14:02:07 +0900

    머신 별 작업 큐 출력

--- simulator/model/machine.py
+++ simulator/model/machine.py
@@ -1,4 +1,5 @@
-# --- simulator/model/machine.py ---
+# simulator/model/machine.py
+
 from simulator.engine.simulator import EoModel, Event
 from simulator.dispatch.dispatch import FIFO
 from simulator.result.recorder import Recorder
@@ -12,17 +13,23 @@ class Machine(EoModel):
         self.queue = deque()
         self.running = None
         self.transfer = transfer_map
-        self.dispatch = FIFO() if dispatch_rule == 'fifo' else FIFO()
+        self.dispatch = FIFO() if dispatch_rule=='fifo' else FIFO()
 
     def handle_event(self, evt):
         et = evt.event_type
-        if et in ('material_arrival', 'part_arrival'):
+        if et in ('material_arrival','part_arrival'):
             part = evt.payload['part']
+            op   = part.job.current_op()
+            # 큐에 들어간 시점도 기록
+            Recorder.log_queue(part, self.name, EoModel.get_time(), op.id, len(self.queue)+1)
             self._enqueue(part)
+
         elif et == 'machine_idle_check':
             self._start_if_possible()
-        elif et in ('end_operation', 'operation_complete'):
-            self._finish()
+
+        elif et in ('end_operation','operation_complete'):
+            op_id = evt.payload.get('operation_id')
+            self._finish(op_id)
 
     def _enqueue(self, part):
         self.queue.append(part)
@@ -30,46 +37,54 @@ class Machine(EoModel):
         self.schedule(ev, 0)
 
     def _start_if_possible(self):
-        if self.status != 'idle' or not self.queue:
+        if self.status!='idle' or not self.queue:
             return
         part = self.dispatch.select(self.queue)
-        op = part.job.current_op()
-        # select_machine이 항상 assigned_machine을 반환하므로 assert 통과
+        op   = part.job.current_op()
         assigned = op.select_machine()
-        assert assigned == self.name
+        assert assigned == self.name, f"Expected {self.name}, got {assigned}"
         dur = op.sample_duration()
-        self.status = 'busy'
+
+        self.status  = 'busy'
         self.running = part
-        part.status = 'processing'
-        Recorder.log_start(part, self.name, EoModel.get_time())
-        ev = Event('end_operation', {'part': part}, dest_model=self.name)
+        part.status  = 'processing'
+        # 시작 시점 기록 (queue_length: 큐에 남은 대기 개수)
+        Recorder.log_start(part, self.name, EoModel.get_time(), op.id, len(self.queue))
+
+        ev = Event('end_operation', {'part': part, 'operation_id': op.id}, dest_model=self.name)
         self.schedule(ev, dur)
 
-    def _finish(self):
+    def _finish(self, op_id=None):
         part = self.running
-        Recorder.log_end(part, self.name, EoModel.get_time())
+        # 종료 시점 기록
+        Recorder.log_end(part, self.name, EoModel.get_time(), op_id)
+
         part.job.advance()
         if part.job.done():
-            # 작업 완료 이벤트 스케줄링
-            Recorder.log_done(part)
-            done_event = Event('job_completed', {'part': part}, dest_model='transducer')
-            self.schedule(done_event, 0)
+            # 완전 종료 기록
+            Recorder.log_done(part, EoModel.get_time())
+            # job_completed 이벤트로 Transducer 호출
+            done_ev = Event('job_completed', {'part': part}, dest_model='transducer')
+            self.schedule(done_ev, 0)
         else:
-            nxt = part.job.current_op().select_machine()
-            spec = self.transfer[nxt]
-            # transfer time 샘플링
-            dist = spec['distribution']
-            if dist == 'normal':
+            # 다음 공정으로 이동
+            nxt  = part.job.current_op().select_machine()
+            spec = self.transfer.get(nxt, {})
+            # 이동 지연 샘플링
+            dist = spec.get('distribution')
+            if dist=='normal':
                 delay = max(0, random.gauss(spec['mean'], spec['std']))
-            elif dist == 'uniform':
+            elif dist=='uniform':
                 delay = random.uniform(spec['low'], spec['high'])
-            elif dist == 'exponential':
+            elif dist=='exponential':
                 delay = random.expovariate(spec['rate'])
             else:
-                raise RuntimeError(f"Unknown transfer distribution for {nxt}")
+                delay = 0
             ev = Event('part_arrival', {'part': part}, dest_model=nxt)
             self.schedule(ev, delay)
+
         self.running = None
-        self.status = 'idle'
+        self.status  = 'idle'
+        # 상태 변화 후에도 큐 체크
         ev = Event('machine_idle_check', dest_model=self.name)
         self.schedule(ev, 0)
--- simulator/result/recorder.py
+++ simulator/result/recorder.py
@@ -8,33 +8,51 @@ class Recorder:
     records = []
 
     @classmethod
-    def log_start(cls, part, machine, time):
+    def log_queue(cls, part, machine, time, operation_id, queue_length):
         cls.records.append({
             'part': part.id,
             'job': part.job.id,
+            'operation': operation_id,
+            'machine': machine,
+            'event': 'queued',
+            'time': time,
+            'queue_length': queue_length
+        })
+
+    @classmethod
+    def log_start(cls, part, machine, time, operation_id, queue_length):
+        cls.records.append({
+            'part': part.id,
+            'job': part.job.id,
+            'operation': operation_id,
             'machine': machine,
             'event': 'start',
-            'time': time
+            'time': time,
+            'queue_length': queue_length
         })
 
     @classmethod
-    def log_end(cls, part, machine, time):
+    def log_end(cls, part, machine, time, operation_id):
         cls.records.append({
             'part': part.id,
             'job': part.job.id,
+            'operation': operation_id,
             'machine': machine,
             'event': 'end',
-            'time': time
+            'time': time,
+            'queue_length': None
         })
 
     @classmethod
-    def log_done(cls, part):
+    def log_done(cls, part, time):
         cls.records.append({
             'part': part.id,
             'job': part.job.id,
+            'operation': None,
             'machine': None,
             'event': 'done',
-            'time': EoModel.get_time()
+            'time': time,
+            'queue_length': None
         })
 
     @classmethod
@@ -42,4 +60,7 @@ class Recorder:
         os.makedirs('results', exist_ok=True)
         df = pd.DataFrame(cls.records)
         df.to_csv('results/trace.csv', index=False)
-        df.to_excel('results/trace.xlsx', index=False)
+        try:
+            df.to_excel('results/trace.xlsx', index=False)
+        except ImportError:
+            pass
